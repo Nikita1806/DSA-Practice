12500d1f8acb64c87ec1242853232aaf249f3f47
Nikita <nikita.20101980@gmail.com>
2026-08-12 14:12:54 +0530

    solved:

--- src/array/RepeatedElement.java
+++ src/array/RepeatedElement.java
@@ -19,7 +19,7 @@ public class RepeatedElement {
 
     public static boolean repeatedElement(int[] arr, int n){
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
+            for(int j = i+1; j<n; j++){
                 if(arr[i]== arr[j]){
                     return true;
                 }
