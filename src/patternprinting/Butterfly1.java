3548b6ff0ea95e3b5f39f4d3a4ca5ef58e018d6b
Nikita <nikita.20101980@gmail.com>
2026-08-14 22:20:16 +0530

    fixed code

--- src/patternprinting/Butterfly1.java
+++ src/patternprinting/Butterfly1.java
@@ -13,7 +13,7 @@ public class Butterfly1 {
                 System.out.print("* ");
             }
 
-            for(int j = 1; j<= n-(2*i); j++){                 //printing spaces
+            for(int j = 1; j<= 2*(n-i); j++){                 //printing spaces
                 System.out.print("  ");
             }
 
@@ -25,18 +25,18 @@ public class Butterfly1 {
 
         }
 
-        for(int i = n; i>=1; i--){                          //lower part
+        for(int i = n-1; i>=1; i--){                          //lower part
 
             for(int j = 1; j<=i; j++){                        //printing first star
                 System.out.print("* ");
             }
 
-            for(int j = 1; j<= n-(2*i); j++){                 //printing spaces
+            for(int j = 1; j<= 2*(n-i); j++){                 //printing spaces
                 System.out.print("  ");
             }
 
             for(int j = 1; j<=i; j++){                        //printing last star
-                System.out.println("* ");
+                System.out.print("* ");
             }
 
             System.out.println();
