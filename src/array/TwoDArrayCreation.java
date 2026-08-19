aaf47e34b77e90f75e982c500c8c118644d470e1
Nikita <nikita.20101980@gmail.com>
2026-08-19 23:51:39 +0530

    fixed code

--- src/array/TwoDArrayCreation.java
+++ src/array/TwoDArrayCreation.java
@@ -13,9 +13,17 @@ public class TwoDArrayCreation {
         System.out.println("Enter the elements of the Array: ");
         int arr[][] = new int[n][m];
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
+            for(int j = 0; j<m; j++){
                 arr[i][j] = sc.nextInt();
             }
+        }
+
+        System.out.println("Your 2D Array: ");
+        for(int i = 0; i<n; i++){
+           for(int j = 0; j<m; j++){
+               System.out.print(arr[i][j] + " ");
+           }
+            System.out.println();
         }
 
     }
