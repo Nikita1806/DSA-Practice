b9c19e35919da5fd77f0d81585fbc085f19be59e
Nikita <nikita.20101980@gmail.com>
2026-08-20 00:19:43 +0530

    Searching in 2D Array

---
+++ src/array/SearchIn2DArray.java
@@ -0,0 +1,44 @@
+package array;
+import java.util.Scanner;
+public class SearchIn2DArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the no. of Rows: ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter the no. of Columns: ");
+        int m = sc.nextInt();
+
+        System.out.println("Enter the elements of the Array: ");
+        int arr[][] = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.print("Enter the element you want to Search: ");
+        int key = sc.nextInt();
+
+        boolean isFound = searchingAnElement(arr,n,m,key);
+        if(isFound){
+            System.out.println("We Have Found Your Element. ");
+        }else{
+            System.out.println("Element not Found in Array.");
+        }
+
+    }
+
+        public static boolean searchingAnElement(int[][] arr, int n, int m, int key){
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (arr[i][j] == key) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+}
