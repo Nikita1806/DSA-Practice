f6d6ebb86d78e37527df33cfe0416ee09f8493e2
Nikita <nikita.20101980@gmail.com>
2026-08-26 22:58:31 +0530

    String Concatenate

--- src/Strings/StringConcatenate.java
+++ src/Strings/StringConcatenate.java
@@ -4,7 +4,7 @@ public class StringConcatenate {
     public static void main(String[] args) {
        String firstName = "Nikita";
        String lastName = "Kumari";
-       String fullname = firstName + " " + lastName + "xyz";  //Concatenate
+       String fullname = firstName + " " + lastName + " " + "xyz";  //Concatenate
 
         System.out.println(fullname);
     }
