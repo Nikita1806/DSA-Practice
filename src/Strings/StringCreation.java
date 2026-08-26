bf34dde414bac75ce77d8114943ce3c9a5c06c75
Nikita <nikita.20101980@gmail.com>
2026-08-26 14:07:18 +0530

    String Creation

--- src/Strings/StringCreation.java
+++ src/Strings/StringCreation.java
@@ -1,6 +1,19 @@
 package Strings;
-import java.util.Scanner;
-public class StringCr
 
+public class StringCreation{
+    public static void main(String[] args) {
+
+        char[] arr1 = new char[5];
+        char[] arr2 = {'H', 'e', 'l', 'l', 'o'};
+
+        String str1 = new String("xyz");
+        String string = "xyz";
+
+        System.out.println(arr1);
+        System.out.println(arr2);
+        System.out.println(str1);
+        System.out.println(string);
+
+    }
 }
 
