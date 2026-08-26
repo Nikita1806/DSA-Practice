fe10041469e87b0bbb347bf92a82192689ece778
Nikita <nikita.20101980@gmail.com>
2026-08-26 22:10:14 +0530

    String Input

---
+++ src/Strings/StringInput.java
@@ -0,0 +1,20 @@
+package Strings;
+import java.util.Scanner;
+public class StringInput {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name;
+        System.out.print("Please Enter Your Name: ");
+        name = sc.next();   //It takes input till first space only
+        sc.nextLine();
+        System.out.println(name);
+
+        String fullName;
+        System.out.print("Please Enter Your Full Name: ");
+        fullName = sc.nextLine();    //It takes input with Spaces
+        System.out.println(fullName);
+
+    }
+}
