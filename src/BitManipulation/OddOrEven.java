ffaa508e16952f6b5f0f0cda164f6674e7a3a338
Nikita <nikita.20101980@gmail.com>
2026-09-19 01:05:48 +0530

    Odd or Even by Bit Manipulation

---
+++ src/BitManipulation/OddOrEven.java
@@ -0,0 +1,23 @@
+package BitManipulation;
+import java.util.Scanner;
+public class OddOrEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a Number: ");
+        int n = sc.nextInt();
+
+
+        oddoreven(n);
+
+    }
+
+    public static void oddoreven(int n){
+        int bitmask = 1;
+        if((n & bitmask) == 1){
+            System.out.println("Odd");
+        }else{
+            System.out.println("Even");
+        }
+    }
+}
