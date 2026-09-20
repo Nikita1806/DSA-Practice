c288c49472fd33a9d492b749890a0f40a5238b60
Nikita <nikita.20101980@gmail.com>
2026-09-20 22:44:29 +0530

    Clear the ith bit of the Number.

--- src/BitManipulation/ClearIthBit.java
+++ src/BitManipulation/ClearIthBit.java
@@ -1,6 +1,6 @@
 package BitManipulation;
 import java.util.Scanner;
-public class SetIthBit {
+public class ClearIthBit {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -11,15 +11,13 @@ public class SetIthBit {
         System.out.print("Enter the bit you wanna Set to 1: ");
         int i = sc.nextInt();
 
-        int result = setIthBitOfNum(num, i);
+        int result = clearIthBitOfNum(num, i);
         System.out.println("After change: " + result);
-
     }
 
-    public static int setIthBitOfNum(int num, int i){
-        int bitmask = 1<<i;
+    public static int clearIthBitOfNum(int num, int i){
+        int bitmask = ~(1<<i);
 
-        return num | bitmask;
+        return num & bitmask;
     }
-
 }
