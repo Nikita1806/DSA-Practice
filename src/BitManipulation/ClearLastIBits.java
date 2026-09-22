ff6f5e393fcd448d95e97e07c3477b3e859b51e9
Nikita <nikita.20101980@gmail.com>
2026-09-22 22:33:14 +0530

    Clear last i bits

---
+++ src/BitManipulation/ClearLastIBits.java
@@ -0,0 +1,23 @@
+package BitManipulation;
+import java.util.Scanner;
+public class ClearLastIBits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a Number: ");
+        int num = sc.nextInt();
+
+        System.out.print("Enter the bit you wanna Update: ");
+        int i = sc.nextInt();
+
+        int result = clearIthBitOfNum(num, i);
+        System.out.println("After change: " + result);
+
+    }
+
+    public static int clearIthBitOfNum(int num, int i) {
+        int bitmask = ~0 << i;
+
+        return num & bitmask;
+    }
+}
